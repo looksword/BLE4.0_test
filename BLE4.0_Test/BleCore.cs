4f91f8f0002b610c491f8573ba9f8baa21b51b31
kangjian.wu@xuanyutech.net <kangjian.wu@xuanyutech.net>
2023-06-29 11:14:38 +0800

    init

---
+++ BLE4.0_Test/BleCore.cs
@@ -0,0 +1,460 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Networking;
+using Windows.Networking.Proximity;
+using Windows.Networking.Sockets;
+using Windows.Security.Cryptography;
+using Windows.Storage.Streams;
+
+namespace BLE4._0_Test
+{
+    public class BleCore
+    {
+        private Boolean asyncLock = false;
+
+        /// <summary>
+        /// 搜索蓝牙设备对象
+        /// </summary>
+        private Windows.Devices.Bluetooth.Advertisement.BluetoothLEAdvertisementWatcher deviceWatcher;
+
+        /// <summary>
+        /// 当前连接的服务
+        /// </summary>
+        public GattDeviceService CurrentService { get; set; }
+
+        /// <summary>
+        /// 当前连接的蓝牙设备
+        /// </summary>
+        public BluetoothLEDevice CurrentDevice { get; set; }
+
+        /// <summary>
+        /// 写特征对象
+        /// </summary>
+        public GattCharacteristic CurrentWriteCharacteristic { get; set; }
+
+        /// <summary>
+        /// 通知特征对象
+        /// </summary>
+        public GattCharacteristic CurrentNotifyCharacteristic { get; set; }
+
+        /// <summary>
+        /// 特性通知类型通知启用
+        /// </summary>
+        private const GattClientCharacteristicConfigurationDescriptorValue CHARACTERISTIC_NOTIFICATION_TYPE = GattClientCharacteristicConfigurationDescriptorValue.Notify;
+
+        /// <summary>
+        /// 存储检测到的设备
+        /// </summary>
+        private List<BluetoothLEDevice> DeviceList = new List<BluetoothLEDevice>();
+
+        /// <summary>
+        /// 定义搜索蓝牙设备委托
+        /// </summary>
+        public delegate void DeviceWatcherChangedEvent(MsgType type, BluetoothLEDevice bluetoothLEDevice);
+
+        /// <summary>
+        /// 搜索蓝牙事件
+        /// </summary>
+        public event DeviceWatcherChangedEvent DeviceWatcherChanged;
+
+        /// <summary>
+        /// 获取服务委托
+        /// </summary>
+        public delegate void GattDeviceServiceAddedEvent(GattDeviceService gattDeviceService);
+
+        /// <summary>
+        /// 获取服务事件
+        /// </summary>
+        public event GattDeviceServiceAddedEvent GattDeviceServiceAdded;
+
+        /// <summary>
+        /// 获取特征委托
+        /// </summary>
+        public delegate void CharacteristicAddedEvent(GattCharacteristic gattCharacteristic);
+
+        /// <summary>
+        /// 获取特征事件
+        /// </summary>
+        public event CharacteristicAddedEvent CharacteristicAdded;
+
+        /// <summary>
+        /// 提示信息委托
+        /// </summary>
+        public delegate void MessAgeChangedEvent(MsgType type, string message, byte[] data = null);
+
+        /// <summary>
+        /// 提示信息事件
+        /// </summary>
+        public event MessAgeChangedEvent MessAgeChanged;
+
+        /// <summary>
+        /// 当前连接的蓝牙Mac
+        /// </summary>
+        private string CurrentDeviceMAC { get; set; }
+
+        public BleCore()
+        {
+
+        }
+
+        /// <summary>
+        /// 搜索蓝牙设备
+        /// </summary>
+        public void StartBleDeviceWatcher()
+        {
+            //string[] requestedProperties = { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected", "System.Devices.Aep.Bluetooth.Le.IsConnectable" };
+            //string aqsAllBluetoothLEDevices = "(System.Devices.Aep.ProtocolId:=\"{bb7bb05e-5972-42b5-94fc-76eaa7084d49}\")";
+
+            //this.deviceWatcher =
+            //        DeviceInformation.CreateWatcher(
+            //            aqsAllBluetoothLEDevices,
+            //            requestedProperties,
+            //            DeviceInformationKind.AssociationEndpoint);
+
+            //// Register event handlers before starting the watcher.
+            //this.deviceWatcher.Added += this.DeviceWatcher_Added;
+            //this.deviceWatcher.Stopped += this.DeviceWatcher_Stopped;
+            //this.deviceWatcher.Start();
+            //string msg = "自动发现设备中..";
+
+            //this.MessAgeChanged(MsgType.NotifyTxt, msg);
+
+            this.deviceWatcher = new Windows.Devices.Bluetooth.Advertisement.BluetoothLEAdvertisementWatcher();
+            this.deviceWatcher.ScanningMode = Windows.Devices.Bluetooth.Advertisement.BluetoothLEScanningMode.Active;
+            this.deviceWatcher.SignalStrengthFilter.InRangeThresholdInDBm = -80;
+            this.deviceWatcher.SignalStrengthFilter.OutOfRangeThresholdInDBm = -90;
+            this.deviceWatcher.SignalStrengthFilter.OutOfRangeTimeout = TimeSpan.FromMilliseconds(5000);
+            this.deviceWatcher.SignalStrengthFilter.SamplingInterval = TimeSpan.FromMilliseconds(2000);
+
+            this.deviceWatcher.Stopped += DeviceWatcher_Stopped;
+            this.deviceWatcher.Received += DeviceWatcher_Received;
+
+            // starting watching for advertisements
+            deviceWatcher.Start();
+            string msg = "自动发现设备中..";
+
+            this.MessAgeChanged(MsgType.NotifyTxt, msg);
+        }
+
+        private void DeviceWatcher_Received(Windows.Devices.Bluetooth.Advertisement.BluetoothLEAdvertisementWatcher sender, Windows.Devices.Bluetooth.Advertisement.BluetoothLEAdvertisementReceivedEventArgs args)
+        {
+            BluetoothLEDevice.FromBluetoothAddressAsync(args.BluetoothAddress).Completed = async (asyncInfo, asyncStatus) =>
+            {
+                if (asyncStatus == AsyncStatus.Completed)
+                {
+                    if (asyncInfo.GetResults() != null)
+                    {
+                        BluetoothLEDevice currentDevice = asyncInfo.GetResults();
+
+                        Boolean contain = false;
+                        foreach (BluetoothLEDevice device in DeviceList)//过滤重复的设备
+                        {
+                            if (device.DeviceId == currentDevice.DeviceId)
+                            {
+                                contain = true;
+                            }
+                        }
+                        if (!contain)
+                        {
+                            this.DeviceList.Add(currentDevice);
+                            this.DeviceWatcherChanged(MsgType.BleDevice, currentDevice);
+                        }
+                    }
+                }
+            };
+        }
+
+        /// <summary>
+        /// 停止搜索蓝牙
+        /// </summary>
+        public void StopBleDeviceWatcher()
+        {
+            this.deviceWatcher.Stop();
+        }
+
+        /// <summary>
+        /// 获取发现的蓝牙设备
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation args)
+        {
+            this.MessAgeChanged(MsgType.NotifyTxt, "发现设备:" + args.Id);
+            this.Matching(args.Id);
+        }
+
+        /// <summary>
+        /// 停止搜索蓝牙设备
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void DeviceWatcher_Stopped(Windows.Devices.Bluetooth.Advertisement.BluetoothLEAdvertisementWatcher sender, Windows.Devices.Bluetooth.Advertisement.BluetoothLEAdvertisementWatcherStoppedEventArgs args)
+        {
+            string msg = "自动发现设备停止";
+            this.MessAgeChanged(MsgType.NotifyTxt, msg);
+        }
+
+        /// <summary>
+        /// 匹配
+        /// </summary>
+        /// <param name="Device"></param>
+        public void StartMatching(BluetoothLEDevice Device)
+        {
+            this.CurrentDevice = Device;
+        }
+
+        /// <summary>
+        /// 获取蓝牙服务
+        /// </summary>
+        public async void FindService()
+        {
+            //var GattServices = this.CurrentDevice.GattServices;
+            //foreach (GattDeviceService ser in GattServices)
+            //{
+            //    this.GattDeviceServiceAdded(ser);
+            //}
+
+            this.CurrentDevice.GetGattServicesAsync().Completed = async (asyncInfo, asyncStatus) =>
+            {
+                if (asyncStatus == AsyncStatus.Completed)
+                {
+                    var services = asyncInfo.GetResults().Services;
+                    this.MessAgeChanged(MsgType.NotifyTxt, "GattServices size=" + services.Count);
+                    foreach (GattDeviceService ser in services)
+                    {
+                        this.GattDeviceServiceAdded(ser);
+                    }
+                    this.MessAgeChanged(MsgType.NotifyTxt, "获取服务收集完毕");
+
+                }
+            };
+        }
+
+        /// <summary>
+        /// 获取特性
+        /// </summary>
+        public async void FindCharacteristic(GattDeviceService gattDeviceService)
+        {
+            //this.CurrentService = gattDeviceService;
+            //foreach (var c in gattDeviceService.GetAllCharacteristics())
+            //{
+            //    this.CharacteristicAdded(c);
+            //}
+
+            this.CurrentService = gattDeviceService;
+            this.CurrentService.GetCharacteristicsAsync().Completed = async (asyncInfo, asyncStatus) =>
+            {
+                if (asyncStatus == AsyncStatus.Completed)
+                {
+                    var characteristics = asyncInfo.GetResults().Characteristics;
+                    this.MessAgeChanged(MsgType.NotifyTxt, "GattCharacteristic size=" + characteristics.Count);
+                    foreach (GattCharacteristic cts in characteristics)
+                    {
+                        this.CharacteristicAdded(cts);
+                    }
+                    this.MessAgeChanged(MsgType.NotifyTxt, "获取特码收集完毕");
+
+                }
+            };
+        }
+
+        /// <summary>
+        /// 获取操作
+        /// </summary>
+        /// <returns></returns>
+        public async Task SetOpteron(GattCharacteristic gattCharacteristic)
+        {
+            if (gattCharacteristic.CharacteristicProperties == GattCharacteristicProperties.Write)
+            {
+                this.CurrentWriteCharacteristic = gattCharacteristic;
+            }
+            if (gattCharacteristic.CharacteristicProperties == GattCharacteristicProperties.Notify)
+            {
+                this.CurrentNotifyCharacteristic = gattCharacteristic;
+            }
+            if ((uint)gattCharacteristic.CharacteristicProperties == 26)
+            { }
+
+            if (gattCharacteristic.CharacteristicProperties == (GattCharacteristicProperties.Notify | GattCharacteristicProperties.Read | GattCharacteristicProperties.Write))
+            {
+                this.CurrentWriteCharacteristic = gattCharacteristic;
+
+                this.CurrentNotifyCharacteristic = gattCharacteristic;
+                this.CurrentNotifyCharacteristic.ProtectionLevel = GattProtectionLevel.Plain;
+                this.CurrentNotifyCharacteristic.ValueChanged += Characteristic_ValueChanged;
+                await this.EnableNotifications(CurrentNotifyCharacteristic);
+            }
+
+            this.Connect();
+        }
+
+        /// <summary>
+        /// 连接蓝牙
+        /// </summary>
+        /// <returns></returns>
+        private async Task Connect()
+        {
+            byte[] _Bytes1 = BitConverter.GetBytes(this.CurrentDevice.BluetoothAddress);
+            Array.Reverse(_Bytes1);
+            this.CurrentDeviceMAC = BitConverter.ToString(_Bytes1, 2, 6).Replace('-', ':').ToLower();
+
+            string msg = "正在连接设备<" + this.CurrentDeviceMAC + ">..";
+            this.MessAgeChanged(MsgType.NotifyTxt, msg);
+            this.CurrentDevice.ConnectionStatusChanged += this.CurrentDevice_ConnectionStatusChanged;
+        }
+
+        /// <summary>
+        /// 搜索到的蓝牙设备
+        /// </summary>
+        /// <returns></returns>
+        private async Task Matching(string Id)
+        {
+            try
+            {
+                BluetoothLEDevice.FromIdAsync(Id).Completed = async (asyncInfo, asyncStatus) =>
+                {
+                    if (asyncStatus == AsyncStatus.Completed)
+                    {
+                        BluetoothLEDevice bleDevice = asyncInfo.GetResults();
+                        this.DeviceList.Add(bleDevice);
+                        this.DeviceWatcherChanged(MsgType.BleDevice, bleDevice);
+                    }
+                };
+            }
+            catch (Exception e)
+            {
+                string msg = "没有发现设备" + e.ToString();
+                this.MessAgeChanged(MsgType.NotifyTxt, msg);
+                this.StartBleDeviceWatcher();
+            }
+        }
+
+        /// <summary>
+        /// 主动断开连接
+        /// </summary>
+        /// <returns></returns>
+        public void Dispose()
+        {
+
+            CurrentDeviceMAC = null;
+            CurrentService?.Dispose();
+            CurrentDevice?.Dispose();
+            CurrentDevice = null;
+            CurrentService = null;
+            CurrentWriteCharacteristic = null;
+            CurrentNotifyCharacteristic = null;
+            MessAgeChanged(MsgType.NotifyTxt, "主动断开连接");
+        }
+
+        private void CurrentDevice_ConnectionStatusChanged(BluetoothLEDevice sender, object args)
+        {
+            if (sender.ConnectionStatus == BluetoothConnectionStatus.Disconnected && CurrentDeviceMAC != null)
+            {
+                string msg = "设备已断开,自动重连";
+                MessAgeChanged(MsgType.NotifyTxt, msg);
+                if (!asyncLock)
+                {
+                    asyncLock = true;
+                    this.CurrentDevice.Dispose();
+                    this.CurrentDevice = null;
+                    CurrentService = null;
+                    CurrentWriteCharacteristic = null;
+                    CurrentNotifyCharacteristic = null;
+                    SelectDeviceFromIdAsync(CurrentDeviceMAC);
+                }
+            }
+            else
+            {
+                string msg = "设备已连接";
+                MessAgeChanged(MsgType.NotifyTxt, msg);
+            }
+        }
+
+        /// <summary>
+        /// 按MAC地址直接组装设备ID查找设备
+        /// </summary>
+        public async Task SelectDeviceFromIdAsync(string MAC)
+        {
+            CurrentDeviceMAC = MAC;
+            CurrentDevice = null;
+            BluetoothAdapter.GetDefaultAsync().Completed = async (asyncInfo, asyncStatus) =>
+            {
+                if (asyncStatus == AsyncStatus.Completed)
+                {
+                    BluetoothAdapter mBluetoothAdapter = asyncInfo.GetResults();
+                    byte[] _Bytes1 = BitConverter.GetBytes(mBluetoothAdapter.BluetoothAddress);//ulong转换为byte数组
+                    Array.Reverse(_Bytes1);
+                    string macAddress = BitConverter.ToString(_Bytes1, 2, 6).Replace('-', ':').ToLower();
+                    string Id = "BluetoothLE#BluetoothLE" + macAddress + "-" + MAC;
+                    await Matching(Id);
+                }
+            };
+        }
+
+        /// <summary>
+        /// 设置特征对象为接收通知对象
+        /// </summary>
+        /// <param name="characteristic"></param>
+        /// <returns></returns>
+        public async Task EnableNotifications(GattCharacteristic characteristic)
+        {
+            string msg = "收通知对象=" + CurrentDevice.ConnectionStatus;
+            this.MessAgeChanged(MsgType.NotifyTxt, msg);
+
+            characteristic.WriteClientCharacteristicConfigurationDescriptorAsync(CHARACTERISTIC_NOTIFICATION_TYPE).Completed = async (asyncInfo, asyncStatus) =>
+            {
+                if (asyncStatus == AsyncStatus.Completed)
+                {
+                    GattCommunicationStatus status = asyncInfo.GetResults();
+                    if (status == GattCommunicationStatus.Unreachable)
+                    {
+                        msg = "设备不可用";
+                        this.MessAgeChanged(MsgType.NotifyTxt, msg);
+                        if (CurrentNotifyCharacteristic != null && !asyncLock)
+                        {
+                            await this.EnableNotifications(CurrentNotifyCharacteristic);
+                        }
+                    }
+                    asyncLock = false;
+                    msg = "设备连接状态" + status;
+                    this.MessAgeChanged(MsgType.NotifyTxt, msg);
+                }
+            };
+        }
+
+        /// <summary>
+        /// 接受到蓝牙数据
+        /// </summary>
+        private void Characteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+        {
+            byte[] data;
+            CryptographicBuffer.CopyToByteArray(args.CharacteristicValue, out data);
+            string str = BitConverter.ToString(data);
+            this.MessAgeChanged(MsgType.BleData, str, data);
+        }
+
+        /// <summary>
+        /// 发送数据接口
+        /// </summary>
+        /// <returns></returns>
+        public async Task Write(byte[] data)
+        {
+            if (CurrentWriteCharacteristic != null)
+            {
+                CurrentWriteCharacteristic.WriteValueAsync(CryptographicBuffer.CreateFromByteArray(data), GattWriteOption.WriteWithResponse);
+                string str = "发送数据：" + BitConverter.ToString(data);
+                this.MessAgeChanged(MsgType.BleData, str, data);
+            }
+
+        }
+    }
+
+    public enum MsgType { NotifyTxt, BleData, BleDevice }
+}
---
+++ BLE4.0_Test/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace BLE4._0_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnServer = new System.Windows.Forms.Button();
+            this.btnFeatures = new System.Windows.Forms.Button();
+            this.btnOpteron = new System.Windows.Forms.Button();
+            this.btnReader = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.cmbServer = new System.Windows.Forms.ComboBox();
+            this.cmbFeatures = new System.Windows.Forms.ComboBox();
+            this.listboxBleDevice = new System.Windows.Forms.ListBox();
+            this.listboxMessage = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(13, 13);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "搜索";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(94, 13);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "匹配";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnServer
+            // 
+            this.btnServer.Location = new System.Drawing.Point(13, 42);
+            this.btnServer.Name = "btnServer";
+            this.btnServer.Size = new System.Drawing.Size(75, 23);
+            this.btnServer.TabIndex = 2;
+            this.btnServer.Text = "获取服务";
+            this.btnServer.UseVisualStyleBackColor = true;
+            this.btnServer.Click += new System.EventHandler(this.btnServer_Click);
+            // 
+            // btnFeatures
+            // 
+            this.btnFeatures.Location = new System.Drawing.Point(13, 71);
+            this.btnFeatures.Name = "btnFeatures";
+            this.btnFeatures.Size = new System.Drawing.Size(75, 23);
+            this.btnFeatures.TabIndex = 3;
+            this.btnFeatures.Text = "获取特征";
+            this.btnFeatures.UseVisualStyleBackColor = true;
+            this.btnFeatures.Click += new System.EventHandler(this.btnFeatures_Click);
+            // 
+            // btnOpteron
+            // 
+            this.btnOpteron.Location = new System.Drawing.Point(13, 100);
+            this.btnOpteron.Name = "btnOpteron";
+            this.btnOpteron.Size = new System.Drawing.Size(75, 23);
+            this.btnOpteron.TabIndex = 4;
+            this.btnOpteron.Text = "获取操作";
+            this.btnOpteron.UseVisualStyleBackColor = true;
+            this.btnOpteron.Click += new System.EventHandler(this.btnOpteron_Click);
+            // 
+            // btnReader
+            // 
+            this.btnReader.Location = new System.Drawing.Point(94, 100);
+            this.btnReader.Name = "btnReader";
+            this.btnReader.Size = new System.Drawing.Size(75, 23);
+            this.btnReader.TabIndex = 5;
+            this.btnReader.Text = "读取";
+            this.btnReader.UseVisualStyleBackColor = true;
+            this.btnReader.Click += new System.EventHandler(this.btnReader_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(175, 100);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // cmbServer
+            // 
+            this.cmbServer.FormattingEnabled = true;
+            this.cmbServer.Location = new System.Drawing.Point(95, 44);
+            this.cmbServer.Name = "cmbServer";
+            this.cmbServer.Size = new System.Drawing.Size(693, 20);
+            this.cmbServer.TabIndex = 7;
+            // 
+            // cmbFeatures
+            // 
+            this.cmbFeatures.FormattingEnabled = true;
+            this.cmbFeatures.Location = new System.Drawing.Point(95, 74);
+            this.cmbFeatures.Name = "cmbFeatures";
+            this.cmbFeatures.Size = new System.Drawing.Size(693, 20);
+            this.cmbFeatures.TabIndex = 8;
+            // 
+            // listboxBleDevice
+            // 
+            this.listboxBleDevice.FormattingEnabled = true;
+            this.listboxBleDevice.ItemHeight = 12;
+            this.listboxBleDevice.Location = new System.Drawing.Point(13, 130);
+            this.listboxBleDevice.Name = "listboxBleDevice";
+            this.listboxBleDevice.Size = new System.Drawing.Size(775, 112);
+            this.listboxBleDevice.TabIndex = 11;
+            // 
+            // listboxMessage
+            // 
+            this.listboxMessage.FormattingEnabled = true;
+            this.listboxMessage.ItemHeight = 12;
+            this.listboxMessage.Location = new System.Drawing.Point(13, 249);
+            this.listboxMessage.Name = "listboxMessage";
+            this.listboxMessage.ScrollAlwaysVisible = true;
+            this.listboxMessage.Size = new System.Drawing.Size(775, 196);
+            this.listboxMessage.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listboxMessage);
+            this.Controls.Add(this.listboxBleDevice);
+            this.Controls.Add(this.cmbFeatures);
+            this.Controls.Add(this.cmbServer);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnReader);
+            this.Controls.Add(this.btnOpteron);
+            this.Controls.Add(this.btnFeatures);
+            this.Controls.Add(this.btnServer);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnServer;
+        private System.Windows.Forms.Button btnFeatures;
+        private System.Windows.Forms.Button btnOpteron;
+        private System.Windows.Forms.Button btnReader;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ComboBox cmbServer;
+        private System.Windows.Forms.ComboBox cmbFeatures;
+        private System.Windows.Forms.ListBox listboxBleDevice;
+        private System.Windows.Forms.ListBox listboxMessage;
+    }
+}
+
